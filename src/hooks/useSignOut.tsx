d32fbbe77d6afcdb21e9a9a20d71dead19c6d52f
Roman Burdeiny <jarkly1483@gmail.com>
2025-11-12 11:11:23 +0300

    adding signout logic

--- src/hooks/useSignOut.tsx
+++ src/hooks/useSignOut.tsx
@@ -1,25 +1,25 @@
-// import { signOut } from "firebase/auth";
-// import { useCallback } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { auth } from "../../firebase-config";
-// import { useDispatch } from "react-redux";
-// import { toast } from "react-toastify";
-// import { logout } from "../redux/auth.slice";
+import { signOut } from 'firebase/auth';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../firebase-config';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { logout } from '../redux/auth.slice';
 
-// const useSignOut = () => {
-//     const navigate = useNavigate()
-//     const dispatch = useDispatch()
+const useSignOut = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-//     const handleSignOut = useCallback(async ()=> {
-//         try{
-//             await signOut(auth)
-//             dispatch(logout())
-//             toast.success('Successfully logged out')
-//             navigate('/login', {replace: true})
-//         } catch (error) {
-//             toast.error(`❌ Error signing out: ?? 'Unknown error'}`)
-//         }
-//     }, [navigate, dispatch])
-//     return handleSignOut
-// }
-// export default useSignOut
+  const handleSignOut = useCallback(async () => {
+    try {
+      await signOut(auth);
+      dispatch(logout());
+      toast.success('Successfully logged out');
+      navigate('/login', { replace: true });
+    } catch (error: unknown) {
+      toast.error(`Error signing out ${(error as Error).message}`);
+    }
+  }, [navigate, dispatch]);
+  return handleSignOut;
+};
+export default useSignOut;
